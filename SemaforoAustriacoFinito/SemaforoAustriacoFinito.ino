2525f1020abdb4f839c51f0e1caad02871894709
Emanuele <emanuele.simionato@barsanti.edu.it>
2019-02-02 10:49:48 +0100

    fix probelama pin 1

--- SemaforoAustriacoFinito/SemaforoAustriacoFinito.ino
+++ SemaforoAustriacoFinito/SemaforoAustriacoFinito.ino
@@ -2,14 +2,14 @@ void Fermi(void)
 {
   digitalWrite(3,HIGH);
   digitalWrite(4,HIGH);
-  digitalWrite(1,LOW);
+  digitalWrite(7,LOW);
   digitalWrite(2,LOW);
   digitalWrite(5,LOW);
   digitalWrite(6,LOW);
 }
 void Fermi1(void)
 {
-  digitalWrite(1,HIGH);
+  digitalWrite(7,HIGH);
   digitalWrite(6,HIGH);
   digitalWrite(2,LOW);
   digitalWrite(3,LOW);
@@ -27,7 +27,7 @@ void lampeggiante(void)
     digitalWrite(4,HIGH);
     delay(1000);   
   }
-  digitalWrite(1,LOW);
+  digitalWrite(7,LOW);
   digitalWrite(5,LOW);
   digitalWrite(6,LOW);
 }
@@ -37,9 +37,9 @@ void lampeggiante1(void)
   digitalWrite(6,HIGH);
   for(int i = 0; i<4;i++)
   {
-    digitalWrite(1,LOW);
+    digitalWrite(7,LOW);
     delay(1000);
-    digitalWrite(1,HIGH);
+    digitalWrite(7,HIGH);
     delay(1000);   
   }
   
@@ -56,12 +56,12 @@ void giallo(void){
 void giallo1(void){
   digitalWrite(2,HIGH);
   digitalWrite(6,HIGH);
-  digitalWrite(1,LOW);
+  digitalWrite(7,LOW);
 }
 
 void setup() {
   // put your setup code here, to run once:
-pinMode(1,OUTPUT);//V1
+pinMode(7,OUTPUT);//V1
 pinMode(2,OUTPUT);//G1
 pinMode(3,OUTPUT);//R1
 pinMode(4,OUTPUT);//V2
@@ -71,9 +71,11 @@ Serial.begin(9600); //opens a serial port, sets data rate to 9600
 Serial.print("seriale attivata");
 }
 
-
+//chiedere all'ultente il delay all'utenete comandi Serial.availabele, Serial.readString(), Seria.print, valore.
 void loop() {
   // put your main code here, to run repeatedly:
+
+
 Fermi();
 delay(5000);
 lampeggiante();
